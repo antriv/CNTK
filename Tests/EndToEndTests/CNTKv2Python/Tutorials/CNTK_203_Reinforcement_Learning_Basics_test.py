2dbc61ae44206271fa9942776e8ebd362931bb7a
Mark Hillebrand <mahilleb@microsoft.com>
2017-04-10 18:30:10 +0200

    CNTK_203_Reinforcement_Learning_Basics_test.py: fix

--- Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_203_Reinforcement_Learning_Basics_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_203_Reinforcement_Learning_Basics_test.py
@@ -11,6 +11,10 @@ abs_path = os.path.dirname(os.path.abspath(__file__))
 notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "CNTK_203_Reinforcement_Learning_Basics.ipynb")
 notebook_timeoutSeconds = 450
 
+def get_output_stream_from_in_cell(cell, stream_name='stdout'):
+    return "\n".join([output.text for output in cell.get('outputs', [])
+        if output.output_type == 'stream' and output.name == stream_name])
+
 def test_cntk_203_reinforcement_learning_basics_noErrors(nb):
     errors = [output for cell in nb.cells if 'outputs' in cell
               for output in cell['outputs'] if output.output_type == "error"]
@@ -18,9 +22,5 @@ def test_cntk_203_reinforcement_learning_basics_noErrors(nb):
     assert errors == []
 
 def test_cntk_203_reinforcement_learning_basics_tasks_are_solved(nb):
-    testCells = [cell for cell in nb.cells
-                 if cell.cell_type == 'code' and
-                     len(cell.outputs) > 0 and
-                     'text' in cell.outputs[0] and
-                     re.search('Task solved in[ :]', cell.outputs[0]['text'])]
+    testCells = [cell for cell in nb.cells if re.search('Task solved in[ :]', get_output_stream_from_cell(cell))]
     assert len(testCells) == 2
